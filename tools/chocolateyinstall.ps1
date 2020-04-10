08c5ec27be1c00620cfc8ce2efbd960a94dfe9a1
Vandre Leal <vandre.leal.candido@gmail.com>
2020-04-10 10:38:39 -0300

    (version) bump to 1.0.0-beta.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'runlet'
 $fileType = 'exe'
-$url = 'https://github.com/runletapp/runlet/releases/download/1.0.0-beta.3/runlet_1.0.0-beta.3_amd64.exe'
+$url = 'https://github.com/runletapp/runlet/releases/download/1.0.0-beta.4/runlet_1.0.0-beta.4_amd64.exe'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
